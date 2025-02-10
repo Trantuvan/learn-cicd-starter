06bec4b7658f6238ed7f95ef43b5cf72d2cd7c30
trantuvan <trantuvan.kan@gmail.com>
2025-02-10 20:54:09 +0700

    fix format auth middleware

--- middleware_auth.go
+++ middleware_auth.go
@@ -11,14 +11,14 @@ type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
 func (cfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		apiKey,err := auth.GetAPIKey(r.Header)
+		apiKey, err := auth.GetAPIKey(r.Header)
 		if err != nil {
 			respondWithError(w, http.StatusUnauthorized, "Couldn't find api key")
 			return
 		}
 
-		user,err := cfg.DB.GetUser(r.Context(), apiKey)
-		if err != nil{
+		user, err := cfg.DB.GetUser(r.Context(), apiKey)
+		if err != nil {
 			respondWithError(w, http.StatusNotFound, "Couldn't get user")
 			return
 		}
